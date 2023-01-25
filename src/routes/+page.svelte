ee95483ae187864aa6c0aac2b78cf1f882f594e1
Glen Chiacchieri <www@glench.com>
2023-01-24 20:45:47 -0500

    minimal repro of error

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { enhance } from '$app/forms';
+</script>
+
+<form action="" method="POST" use:enhance>
+    <input type="submit" />
+</form>
---
+++ src/routes/page.server.js
@@ -0,0 +1,10 @@
+import type { Actions } from './$types';
+
+const SALT_ROUNDS = 10;
+
+export const actions: Actions = {
+  default: async ({request, locals}) => {
+    console.log('hlskdjflsdjflsdjfljsldfjlsdjflsdjfslkdjfldskjfslj<')
+  }
+}
+
